86a7148f0744c7e42e36c5fc0716ac4432651421
Kauany Costa <117752501+kauanycosta@users.noreply.github.com>
2023-06-27 22:53:04 -0300

    Update auxiliaraula03.py
    Adequações nos nomes dos arquivos.

--- auxiliaraula03.py
+++ auxiliaraula03.py
@@ -5,39 +5,39 @@ def nomeando(dia, mes, ano):
     match mes:
         case '01':
             nomeMes = "Janeiro"
-            aula21junho.mostrar(dia, nomeMes, ano)
+            aula03.mostrar(dia, nomeMes, ano)
         case '02':
             nomeMes = "Fevereiro"
-            aula21junho.mostrar(dia, nomeMes, ano)
+            aula03.mostrar(dia, nomeMes, ano)
         case '03':
             nomeMes = "Março"
-            aula21junho.mostrar(dia, nomeMes, ano)
+            aula03.mostrar(dia, nomeMes, ano)
         case '04': 
             nomeMes = "Abril"
-            aula21junho.mostrar(dia, nomeMes, ano)
+            aula03.mostrar(dia, nomeMes, ano)
         case '05': 
             nomeMes = "Maio"
-            aula21junho.mostrar(dia, nomeMes, ano)
+            aula03.mostrar(dia, nomeMes, ano)
         case '06':
             nomeMes = "Junho"
-            aula21junho.mostrar(dia, nomeMes, ano)
+            aula03.mostrar(dia, nomeMes, ano)
         case '07':
             nomeMes = "Julho"
-            aula21junho.mostrar(dia, nomeMes, ano)
+            aula03.mostrar(dia, nomeMes, ano)
         case '08': 
             nomeMes = "Agosto"
-            aula21junho.mostrar(dia, nomeMes, ano)
+            aula03.mostrar(dia, nomeMes, ano)
         case '09': 
             nomeMes = "Setembro"
-            aula21junho.mostrar(dia, nomeMes, ano)
+            aula03.mostrar(dia, nomeMes, ano)
         case '10': 
             nomeMes = "Outubro"
-            aula21junho.mostrar(dia, nomeMes, ano)
+            aula03.mostrar(dia, nomeMes, ano)
         case '11': 
             nomeMes = "Novembro"
-            aula21junho.mostrar(dia, nomeMes, ano)
+            aula03.mostrar(dia, nomeMes, ano)
         case '12': 
             nomeMes = "Dezembro"
-            aula21junho.mostrar(dia, nomeMes, ano)
+            aula03.mostrar(dia, nomeMes, ano)
         case _:
             print('Mês inválido')
