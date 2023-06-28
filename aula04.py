fad19dd778396d9451e94da4caefbdec450482f3
Kauany Costa <117752501+kauanycosta@users.noreply.github.com>
2023-06-28 15:56:21 -0300

    Create aula04.py
    Aula do dia 23 de junho.

---
+++ aula04.py
@@ -0,0 +1,27 @@
+# ----------------------------------------------------------------------------------------------------- //
+# 1 - Fazer um programa que converta a temperatura digitada pelo usuário para celsius ou para 
+# fahrenhardt dependendo da opção usada
+# ----------------------------------------------------------------------------------------------------- //
+import auxiliar04
+def principal():
+    temp = float(input("Informe a temperatura: "))
+    opcao = int(input("Converter para:\n(1) Fahrenhardt\n(2) Celsius\nEscolha: "))
+    auxiliar04.calcular(temp, opcao)
+if(__name__=='__main__'):
+    principal()
+
+def mostrar(tempNova, tempLiteral):
+    print(f"A sua temperatura convertida para {tempLiteral} é {tempNova}")
+
+# ----------------------------------------------------------------------------------------------------- //
+# 2 - Escreva um programa que peça ao usuário um valor de 500 a 1000, controlar essa entrada.
+# Se o número for par, multiplicar por ele mesmo. Se for impar, fazer a raiz quadrada.
+# ----------------------------------------------------------------------------------------------------- //
+
+def principal2():
+    valor = 501
+    while(valor>=500 and valor<=1000):
+        valor = float(input("Escolhe um valor que esteja entre o intervalo de 500 até 100.\nInforme o valor: "))
+        auxiliar04.calcular2(valor)
+if(__name__=="__main__"):
+    principal2()
