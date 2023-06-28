ad0f0da3f4a796c6ceb11b9ddfc1a5a852578e3b
Kauany Costa <117752501+kauanycosta@users.noreply.github.com>
2023-06-28 15:58:53 -0300

    Update aula04.py
    Alterando onde havia o nome antigo do arquivo auxiliaraula04

--- aula04.py
+++ aula04.py
@@ -2,11 +2,11 @@
 # 1 - Fazer um programa que converta a temperatura digitada pelo usuário para celsius ou para 
 # fahrenhardt dependendo da opção usada
 # ----------------------------------------------------------------------------------------------------- //
-import auxiliar04
+import auxiliaraula04
 def principal():
     temp = float(input("Informe a temperatura: "))
     opcao = int(input("Converter para:\n(1) Fahrenhardt\n(2) Celsius\nEscolha: "))
-    auxiliar04.calcular(temp, opcao)
+    auxiliaraula04.calcular(temp, opcao)
 if(__name__=='__main__'):
     principal()
 
@@ -22,6 +22,6 @@ def principal2():
     valor = 501
     while(valor>=500 and valor<=1000):
         valor = float(input("Escolhe um valor que esteja entre o intervalo de 500 até 100.\nInforme o valor: "))
-        auxiliar04.calcular2(valor)
+        auxiliaraula04.calcular2(valor)
 if(__name__=="__main__"):
     principal2()
